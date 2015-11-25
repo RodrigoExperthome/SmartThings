364744e378f5a5f661b419ed9bd297ec6bc3ca12
Experthome <rcuadros@gmail.com>
2015-11-25 01:14:12 -0300

    Update Expert Lights.groovy

--- SmartApps/Expert Lights.groovy
+++ SmartApps/Expert Lights.groovy
@@ -66,7 +66,7 @@ def pageSetup() {
         type:       "bool",
         title:      "Override estado luz?",
         multiple:   false,
-        required:   true
+        required:   true,
         defaultValue: true
     ]
    def pageProperties = [
@@ -134,27 +134,25 @@ private def onHandler (evt, sensorType) {
             	log.debug("Cancelando proceso de delay, dado que aparecio nuevo evento")
             	unschedule(apagarLuz)
             	state.timerStart = false
+            	state.processNumber = state.processNumber + 1
         	} else {
-        		
+        		if (state.processNumber == 0) {
+					log.debug("Primer proceso de activacion")
+					def offLuces = settings.luces.findAll {it?.latestValue("switch").contains("off")}
+            		log.debug("Las luces apagadas al iniciar activacion son : '${offLuces}'")             	
+            		state.lucesOff = offLuces.collect{it.id}
+                	offLuces*.on()
+                	state.processNumber = state.processNumber + 1
+                	if (sensorType == "presence") {
+                		log.debug("Proceso de presencia con delay obligado")   
+                    	runIn(delay * 60, "apagarLuz")
+                		state.timerStart = true		
+                	}
+				} else {
+					state.processNumber = state.processNumber + 1
+					log.debug("Proceso ${state.processNumber} de activacion")
+				}
         	}
-			
-			
-			if (state.processNumber == 0) {
-				log.debug("Primer proceso de activacion")
-				def offLuces = settings.luces.findAll {it?.latestValue("switch").contains("off")}
-                log.debug("Las luces apagadas al iniciar activacion son : '${offLuces}'")             	
-            	state.lucesOff = offLuces.collect{it.id}
-                offLuces*.on()
-                state.processNumber += 1
-                if (sensorType = "presence") {
-                	log.debug("Proceso de presencia con delay obligado")   
-                    runIn(delay * 60, "apagarLuz")
-                	state.timerStart = true		
-                }
-			} else {
-				log.debug("Proceso ${state.processNumber} de activacion")
-				state.processNumber += 1
-			}
 		} else {
 			if (state.processNumber == 1) {
 				if (settings.delay) {
@@ -163,16 +161,11 @@ private def onHandler (evt, sensorType) {
                 	state.timerStart = true
             	}		
 			}
-			state.processNumber -= 1
+			state.processNumber = state.processNumber - 1
+			log.debug("Proceso ${state.processNumber} de activacion")
 		}
-		
 	
-	
-		
 	}
-	
-	
-	
 }
 
 def inputOk (device, sensorType) {
@@ -192,103 +185,15 @@ def inputOk (device, sensorType) {
 	}	
 }
 
-
-
-//Cuando ocurre un evento, y aparece otro antes de que se desactive...
-//state.offluces queda vacio, dado que primer evento las prendio.
-def onEvento(evt) {
-	if (!modo || modo.contains(location.mode))  {
-    	if (getInputOk(movimiento, contacto, presencia)) {
-    		log.debug("Movimiento, Contacto o Presencia detectada")           
-            if (state.timerStart){
-            	log.debug("Cancelando proceso de delay, dado que aparecio nuevo evento")
-            	unschedule(apagarLuz)
-            	state.timerStart = false
-                state.killedProcess = true
-        	} else {
-        		//Me estoy quedando atrapado por el loop killedProcess
-            	if (!state.killedProcess) {
-       	            def offLuces = settings.luces.findAll {it?.latestValue("switch").contains("off")}
-                    log.debug("Las luces apagadas al iniciar la app son : '${offLuces}'")             	
-            	    state.lucesOff = offLuces.collect{it.id}
-                    offLuces*.on()
-                    //Eventos de presencia no tienen cierre, por tanto deben tener delay.
-                    if (getPresence(presencia)) {
-                    	log.debug("Proceso de presencia con delay obligado")   
-                    	runIn(delay * 60, "apagarLuz")
-                		state.timerStart = true	
-                    }
-                }
-            }
-      	} else {
-        	if (settings.delay) {
-            	log.debug("Activando proceso de delay '${delay}' minutos ")
-            	if (state.killedProcess) {
-                	log.debug("Proceso original no terminado... luces a apagar no cambian ")  
-                }
-                runIn(delay * 60, "apagarLuz")
-                state.timerStart = true
-            } else {
-                log.debug("Apagando luz inmediatamente (no hay delay)")
-                apagarLuz()
-         	}
-		}
-    } else {
-    	log.debug("Detectado pero no se activa debido a restricciones de modo")
-    }
-}
-
-//Procedimientos comunes
-private getInputOk(motion, contact, presence) {
-	def motionDetected = false
-	def contactDetected = false
-	def presenceDetected = false
-	def result = false
-
-	if (motion) {
-		if (motion.latestValue("motion").contains("active")) {
-			motionDetected = true
-            log.debug("Motion valor = ${motion.latestValue("motion")}")
-		}
-	}
-	if (contact) {
-		if (contact.latestValue("contact").contains("open")) {
-			contactDetected = true
-            log.debug("Contact valor = ${contact.latestValue("contact")}")
-		}
-	}
-	if (presence) {
-		if (presence.latestValue("presence").contains("present")) {
-			presenceDetected = true
-            log.debug("Presence valor = ${presence.latestValue("presence")}")
-		}
-	}
-	result = motionDetected || contactDetected || presenceDetected
-	result
-}
-
-private getPresence(presence) {
-	def presenceDetected = false
-	def result = false
-	if (presence) {
-		if (presence.latestValue("presence").contains("present")) {
-			presenceDetected = true
-            log.debug("Presence valor = ${presence.latestValue("presence")}")
-		}
-	}
-	result = presenceDetected
-	result
-}
-
-private apagarLuz() {
+private def apagarLuz() {
 	def lucesOn_Off = state.lucesOff
 	log.debug ("Luces a apagar son: '${lucesOn_Off}'")
-    	settings.luces.each() {
+    settings.luces.each() {
     	if (lucesOn_Off.contains (it.id)){
         	it.off()
-        }
-    }
+    	}
+   	}
+   	state.lucesOff = []
     state.timerStart = false
-    state.killedProcess = false
 }
 
