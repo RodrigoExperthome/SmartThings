cc88e2687f63815c365bc048b1f9665c7ef44ccf
Experthome <rcuadros@gmail.com>
2015-12-07 10:49:45 -0300

    Update Expert Alarm.groovy

--- SmartApps/Expert Alarm.groovy
+++ SmartApps/Expert Alarm.groovy
@@ -21,7 +21,7 @@
  *  You should have received a copy of the GNU General Public License along
  *  with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- *  
+ *  To Do: (i) Armada Afuera no verifica al momento de armar si hay contactos abiertos.
  * 
  * 
  */
